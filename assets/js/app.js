1fb2eb1622ad3a83722b818136e3534e7c0a91fe
Safiya2358 <130473668+Safiya2358@users.noreply.github.com>
2024-06-18 15:01:09 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+// Function to load navbar and footer
+$(function () {
+  $("#footer").load("assets/HTML/footer.html");
+});
+
+$(function () {
+  $("#navbar").load("assets/HTML/navbar.html");
+});
+
+// Back to Top button
+var btn = $("#backToTop");
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 0) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
+
+// Nav-Links on-click colour
+function home() {
+  document.getElementById("home").style.color = "#1eff8f";
+  document.getElementById("about").style.color = "white";
+  document.getElementById("contact").style.color = "white";
+}
+
+function about() {
+  document.getElementById("about").style.color = "#1eff8f";
+  document.getElementById("home").style.color = "white";
+  document.getElementById("contact").style.color = "white";
+}
+
+function contact() {
+  document.getElementById("contact").style.color = "#1eff8f";
+  document.getElementById("home").style.color = "white";
+  document.getElementById("about").style.color = "white";
+}
